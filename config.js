823abb646a366722499c1ee8e3e85e1aa43d76b6
Hm <110269965+FachriBotV4@users.noreply.github.com>
2022-08-23 14:57:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281390658325'],
-  ['6282256115584'],
-  ['6281319944687', 'BG', true]
+  ['6285713041886'],
+  ['6285713041886'],
+  ['6285713041886', 'Fachri', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
